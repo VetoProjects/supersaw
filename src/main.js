f33ea29d13b7cfb63a63e69dc9fb8f83c6414332
Tobsic <s0539713@htw-berlin.de>
2015-04-16 12:57:37 +0000

    updated main.js to use form and minor fix

--- src/main.js
+++ src/main.js
@@ -1,61 +1,61 @@
 var myMixer = mixer();
 
-document.querySelector('#player1 .audioFile').addEventListener(
+var player1 = document.getElementById('player1');
+var player2 = document.getElementById('player2');
+
+player.audioFile.addEventListener(
     'change',
     function(e) { myMixer.player1.load(this.files[0]); },
     false
 );
-document.querySelector('#player1 .volume').addEventListener(
+layer1.volume.addEventListener(
     'input',
     function(e) { myMixer.player1.volume(this.value / this.max); },
     false
 );
-document.querySelector('#player1 .high').addEventListener(
+player1.high.addEventListener(
     'input',
     function(e) { myMixer.player1.high(this.value / this.max); },
     false
 );
-document.querySelector('#player1 .middle').addEventListener(
+layer1.middle.addEventListener(
     'input',
     function(e) { myMixer.player1.middle(this.value / this.max); },
     false
 );
-document.querySelector('#player1 .low').addEventListener(
+player1.low.addEventListener(
     'input',
     function(e) { myMixer.player1.low(this.value / this.max); },
     false
 );
 
-
-
-document.querySelector('#player2 .audioFile').addEventListener(
+player2.audioFile.addEventListener(
     'change',
     function(e) { myMixer.player2.load(this.files[0]); },
     false
 );
-document.querySelector('#player2 .volume').addEventListener(
+player2.volume.addEventListener(
     'input',
     function(e) { myMixer.player2.volume(this.value / this.max); },
     false
 );
-document.querySelector('#player2 .high').addEventListener(
+player2.high.addEventListener(
     'input',
     function(e) { myMixer.player2.high(this.value / this.max); },
     false
 );
-document.querySelector('#player2 .middle').addEventListener(
+player2.middle.addEventListener(
     'input',
     function(e) { myMixer.player2.middle(this.value / this.max); },
     false
 );
-document.querySelector('#player2 .low').addEventListener(
+player2.low.addEventListener(
     'input',
     function(e) { myMixer.player2.low(this.value / this.max); },
     false
 );
 
-
-document.querySelector('#crossfader').addEventListener(
+document.getElementById('crossfader').addEventListener(
     'input',
     function(e) { myMixer.crossfader.fade((this.value / this.max) * 2 - 1); },
     false
